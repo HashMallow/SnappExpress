f6c2a88798abfcc09b8f114b2cdd77482d4b570e
unknown <mohsen.fatemi73@gmail.com>
2024-08-20 23:54:43 +0330

    1.0.0.2 added first version of codes for vendor products

--- test.py
+++ test.py
@@ -1,222 +1,149 @@
-import requests
+﻿import requests
 import json
-import argparse
-import numpy as np
-import time
-from threading import Thread
-from queue import Queue
-import pandas as pd
 
-class Worker(Thread):
-    """ Thread executing tasks from a given tasks queue """
+# Define the request session
+session = requests.Session()
+session.headers.update({
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "authority": "snapp.express",
+    "method": "POST",
+    "path": "/supermarket/%D8%A7%D8%B3%D9%85%D8%A7%D8%B1%D8%AA-%DB%8C%D9%88%D8%B3%D9%81-%D8%A2%D8%A8%D8%A7%D8%AF-(%D8%B3%D9%88%D9%BE%D8%B1%D9%85%D8%A7%D8%B1%DA%A9%D8%AA-%D8%B2%D9%86%D8%AC%DB%8C%D8%B1%D9%87-%D8%A7%DB%8C-%D8%A7%D8%B3%D9%86%D9%BE)%2B/api",
+    "scheme": "https",
+    "accept": "*/*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.6",
+    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsibW9iaWxlX3YyIiwibW9iaWxlX3YxIiwid2VidmlldyJdLCJreWMiOjAsInVzZXJDb2RlIjpudWxsLCJzdWJzaWR5IjowLCJ1ZGlkIjoiMzQ4NDIzOTgtNzhlYi00ZjEwLWEzYWUtYWQ0MDRjYzM4YTQ4IiwiYXVkIjoic25hcHBmb29kX3B3YSIsImV4cCI6MTcyNDM5ODUyMywibmJmIjoxNzI0MTM5MjAzLCJpYXQiOjE3MjQxMzkyMDMsImp0aSI6ImY1MTdiZWQyLWZkOGYtNDVkOC1hZTk3LTI2ODhkMjIzNDY5YSIsInN1YiI6IiJ9.W7k_xqTsbL-5G8v0M77-Cpfu-I-cWJ7BerSKI7hFgNfW5EGkuIAAyiBbqcXAn85SqWvrJdojtmEfvl8FIpzUF1f5PmUNauUB-H-x_HQB3oKzIBnPXMZp8wEhLti5DimF38wHMXkMBY5dNc46anReo6BR2lTS1RYEp6ek-8OBUNoZHOlHgPXOJCvFZsay-nBD4nw5GdxtoWNm9miYqy0MO3pSuy6f_mo25s3IYh5iAjjPJo25-rw5WBLD3Vl-rzHAqw2dkconPlfhFo4L36BMNxO_BaNNJARA32jyY_hyBQm7FX967nRkFIwAIn6nDVQTpMwTl40Ve1T9rO7gv8yhyw",
+    "origin": "https://snapp.express",
+    "priority": "u=1, i",
+    "referer": "https://snapp.express/supermarket/%D8%A7%D8%B3%D9%85%D8%A7%D8%B1%D8%AA-%DB%8C%D9%88%D8%B3%D9%81-%D8%A2%D8%A8%D8%A7%D8%AF-(%D8%B3%D9%88%D9%BE%D8%B1%D9%85%D8%A7%D8%B1%DA%A9%D8%AA-%D8%B2%D9%86%D8%AC%DB%8C%D8%B1%D9%87-%D8%A7%DB%8C-%D8%A7%D8%B3%D9%86%D9%BE)%2B/0yjd7m",
+    "sec-ch-ua": '"Not)A;Brand";v="99", "Brave";v="127", "Chromium";v="127"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1",
+    "traceparent": "00-1b5e5fffe7a496071dbad9b64d3cbfe7-f547dbf14b0d852d-01",
+    "x-metadata": '{"client":"PWA","optionalClient":"PWA","deviceType":"PWA","appVersion":"5.6.6","clientVersion":"a4547bd9","optionalVersion":"5.6.6","UDID":"34842398-78eb-4f10-a3ae-ad404cc38a48","lat":"35.727","long":"51.393"}'
+})
 
-    def __init__(self, tasks):
-        Thread.__init__(self)
-        self.tasks = tasks
-        self.daemon = True
-        self.start()
-
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try:
-                func(*args, **kargs)
-            except Exception as e:
-                # An exception happened in this thread
-                print(e)
-            finally:
-                # Mark this task as done, whether an exception happened or not
-                self.tasks.task_done()
-
-
-class ThreadPool:
-    """ Pool of threads consuming tasks from a queue """
-
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
-        for _ in range(num_threads):
-            Worker(self.tasks)
-
-    def add_task(self, func, *args, **kargs):
-        """ Add a task to the queue """
-        self.tasks.put((func, args, kargs))
-
-    def map(self, func, args_list):
-        """ Add a list of tasks to the queue """
-        for args in args_list:
-            self.add_task(func, args)
-
-    def wait_completion(self):
-        """ Wait for completion of all the tasks in the queue """
-        self.tasks.join()
-
-parser = argparse.ArgumentParser(description="SnappExpress Killer")
-parser.add_argument("-l1", "--lat1", type=float,default=35.82353534833643, help="latitude")
-parser.add_argument("-t1","--lon1", type=float,default=51.088133139649216
-, help="longitude")
-parser.add_argument("-l2", "--lat2", type=float,default=35.5830273127383, help="latitude")
-parser.add_argument("-t2","--lon2", type=float,default=51.59405717956719, help="longitude")
-args = parser.parse_args()
-
-url = "https://snapp.express/vendor-list/api"
-
-latitudes = np.linspace(args.lat1,args.lat2,100)
-longitudes = np.linspace(args.lon1,args.lon2,120)
-
-payload = {
-    "operationName": "getVendorList",
+# Define the request body
+request_body = {
+    "operationName": "getSuperMarketProductList",
     "variables": {
-        "variable": "-1",
-        "page": 0,
-        "pageSize": 100,
-        "filters": {
-            "superType": [4],
-            "mode": "CURRENT",
-            "item_position": "homePage"
-        }
+        "variable": 1172,
+        "secondVariable": "0yjd7m",
+        'page':5,
+        'pageSize':10
     },
     "query": """
-    query getVendorList($variable: String, $page: Int, $pageSize: Int, $filters: JSONObject) {
-      vendorList(
-        variable: $variable
-        page: $page
-        pageSize: $pageSize
-        filters: $filters
-      ) {
-        status
-        data {
-          count
-          openCount
-          extraSections {
-            filters {
-              top {
+        query getSuperMarketProductList($variable: Int, $secondVariable: String, $page: Int, $pageSize: Int) {
+            superMarketProductList(
+                variable: $variable
+                secondVariable: $secondVariable
+                page: $page
+                pageSize: $pageSize
+            ) {
+                status
                 data {
-                  title
-                  value
-                  __typename
+                    id
+                    title
+                    count
+                    extraSections {
+                        filters {
+                            top {
+                                data {
+                                    title
+                                    value
+                                    __typename
+                                }
+                                __typename
+                            }
+                            sections {
+                                sectionNameFa
+                                data {
+                                    title
+                                    value
+                                    __typename
+                                }
+                                __typename
+                            }
+                            __typename
+                        }
+                        __typename
+                    }
+                    finalResult {
+                        id
+                        type
+                        data {
+                            id
+                            title
+                            score
+                            rating
+                            type
+                            discount
+                            discountRatio
+                            description
+                            price
+                            vendorLogo
+                            highlight
+                            popularityBadgeUrl
+                            commentCount
+                            isEcommerce
+                            noStock
+                            brand
+                            menuCategoryId
+                            stock
+                            capacity
+                            images {
+                                thumb
+                                __typename
+                            }
+                            vendor {
+                                title
+                                code
+                                vendorCode
+                                category
+                                rating
+                                highlight
+                                description
+                                commentCount
+                                address
+                                vendorType
+                                isOpen
+                                isPreorderEnabled
+                                isExpress
+                                deliveryFee
+                                deliveryTime
+                                featured
+                                minOrder
+                                hasCoupon
+                                couponCount
+                                deliver
+                                bestCoupon
+                                paymentTypes
+                                __typename
+                            }
+                            __typename
+                        }
+                        __typename
+                    }
+                    __typename
                 }
                 __typename
-              }
-              sections {
-                data {
-                  title
-                  value
-                  __typename
-                }
-                __typename
-              }
-              __typename
             }
-            __typename
-          }
-          finalResult {
-            data {
-              id
-              title
-              isMarketParty
-              backgroundImage
-              commentCount
-              minimumOrderValue
-              code
-              status
-              area
-              countReview
-              isExpressPin
-              logo
-              isOpen
-              preOrderEnabled
-              deliveryFee
-              deliveryTime
-              discountValueForView
-              rating
-
-              __typename
-            }
-            type
-            __typename
-          }
-          __typename
         }
-        __typename
-      }
-    }
     """
 }
 
-def scrape(item):
-    global dicts, proxies
-    i,lat,lon = item
-    print(i,'/',12000)
-    headers = {
-        "User-Agent": "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-        "Accept": "*/*",
-        "Content-Type": "application/json",
-        "x-metadata": json.dumps({
-            "client": "PWA",
-            "optionalClient": "PWA",
-            "lat": f"{lat}",
-            "long": f"{lon}"
-        }),
-    }
+# Send the request
+response = session.post("https://snapp.express/supermarket/%D8%A7%D8%B3%D9%85%D8%A7%D8%B1%D8%AA-%DB%8C%D9%88%D8%B3%D9%81-%D8%A2%D8%A8%D8%A7%D8%AF-(%D8%B3%D9%88%D9%BE%D8%B1%D9%85%D8%A7%D8%B1%DA%A9%D8%AA-%D8%B2%D9%86%D8%AC%DB%8C%D8%B1%D9%87-%D8%A7%DB%8C-%D8%A7%D8%B3%D9%86%D9%BE)%2B/api", json=request_body)
 
-    un = True
-    while un:
-        try:
-            response = requests.post(url, headers=headers, json=payload,proxies=proxies)
-            if response.status_code == 200:
-                un=False
-        except:
-            pass
-        time.sleep(0.5)
-
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        dicts[i] = []
-        data = response.json()
-        # Now you can work with the data
-        for it in data['data']['vendorList']['data']['finalResult']:
-            dicts[i].append(it)
-            print(it['data']['title'])
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-        print(response.text)
-
-dicts = {}
-cnt = 1
-items = []
-for latitude in latitudes:
-    for longitude in longitudes:
-        items.append((cnt,latitude,longitude))
-        cnt+=1
-
-# Proxy information
-proxy_domain = "p.webshare.io"
-proxy_port = "80"
-proxy_username = "velrbxxn-rotate"
-proxy_password = "np1basaqv9ok"
-
-# Construct the proxy URL
-proxy_url = f"http://{proxy_username}:{proxy_password}@{proxy_domain}:{proxy_port}"
-
-# Set up the proxy dictionary
-proxies = {
-    "http": proxy_url,
-    "https": proxy_url
-}
-pool = ThreadPool(40)
-pool.map(scrape, items)
-pool.wait_completion()
-
-final_list = []
-for item in dicts:
-    final_list += dicts[item]
-
-dk = {}
-for i in range(len(final_list)):
-    dk[i] = final_list[i]['data']
-
-df = pd.DataFrame(dk).T
-df.drop_duplicates(inplace=True)
-df.reset_index(drop=True, inplace=True)
-df.to_csv('Express_Vendors_Tehran.csv', index=False, encoding='utf-8-sig')
+# Check the response status code
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    # print(json.dumps(data, indent=2))
+    print(len(data['data']['superMarketProductList']['data']['finalResult']))
+else:
+    print(f"Request failed with status code {response.status_code}")
